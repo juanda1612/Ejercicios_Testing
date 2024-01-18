52f66284534d1ff6181231053e2b1d766e16b277
juanda1612 <juan.perez77@educa.madrid.org>
2024-01-18 20:07:06 +0100

    asdasd

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+L
+src/main/java/HolaMundo.java,c/6/c66ec77952ca50489f93eeb99c1ed58cfdd21bbb
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+X
+(src/main/java/Ejercicio01/Ejercicio01.md,7/8/78c1464f140a3efb0acfeae3d37732963e15dde1
+[
++src/test/java/Ejercicio01/FizzBuzzTest.java,e/9/e9982af9ae4a396b0ceee6f2ecbbf92296209f3d
+P
+ src/test/java/HolaMundoTest.java,4/7/4720c764265442fdd8903218e82ef03c0ff28216
+W
+'src/main/java/Ejercicio01/FizzBuzz.java,c/d/cd54a79292e224a1f8b22f471f62c2860414ea01
+X
+(src/main/java/Ejercicio02/Ejercicio02.md,f/d/fdc40c3cb66d3c1502564bf2e454c1b50fde289d
+X
+(src/main/java/Ejercicio02/CamelCase.java,a/a/aa7fc4732b1748fc2978598de7b399b3cf82d89f
--- src/main/java/Ejercicio02/CamelCase.java
+++ src/main/java/Ejercicio02/CamelCase.java
@@ -1,9 +1,32 @@
 package Ejercicio02;
+/*
+Camel Case
+Completa el método de manera que convierta palabras delimitadas por guiones/barras bajas en notación camel case.
+
+La primera palabra en la salida debe estar en mayúsculas solo si la palabra original estaba en mayúsculas (conocido como Upper Camel Case, también a menudo referido como Pascal case). Las palabras que vengan después siempre deben comenzar en mayúscula.
+
+Ejemplos: Examples "el-guerrero-sigiloso" se convierte en "elGuerreroSigiloso"
+
+"El-Guerrero_Sigiloso" se convierte en "ElGuerreroSigiloso"
+
+"El_Guerrero_Sigiloso" se convierte en "ElGuerreroSigiloso"
+ */
+
+import java.util.Scanner;
 
 public class CamelCase {
+    static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) {
         // solución
-        camelCase("el-guerrero-sigiloso");
+       String palabra = teclado.nextLine();
+        for (int i = 0; i < palabra.length(); i++) {
+            if (palabra.contains("-")){
+                palabra.replace("-", " ");
+            }
+        }
+        System.out.println(palabra);
+
+
     }
 
     public static String camelCase(String texto){
