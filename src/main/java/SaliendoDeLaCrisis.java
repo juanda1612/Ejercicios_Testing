e35d04e3708dfb04f7c75c3780f7844d6e8013f4
juanda1612 <juan.perez77@educa.madrid.org>
2024-03-18 16:24:13 +0100

    asdasd

---
+++ src/main/java/SaliendoDeLaCrisis.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SaliendoDeLaCrisis {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int numeroLinea = teclado.nextInt();
+        if (numeroLinea >= 3){
+            while (numeroLinea != 0){
+               String cadena = teclado.nextLine();
+               String[] numeros = cadena.split(" ");
+               int[] numerosInt;
+            }
+        }else {
+            System.out.println("numero no valido");
+        }
+    }
+}
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -8,4 +8,41 @@ class CalculadoraTest {
     void sumar() {
         assertEquals(4, Calculadora.sumar(2,2)); // caso de prueba
     }
+
+    @Test
+    void sumar2() {
+        assertEquals(0, Calculadora.sumar(2,-2)); // caso de prueba
+    }
+
+    @Test
+    void restar() {
+        assertEquals(-14000,Calculadora.restar(1000,15000));
+    }
+
+    @Test
+    void restar2() {
+        assertEquals(-33,Calculadora.restar(-3,30));
+    }
+
+
+
+    @Test
+    void multiplicar() {
+        assertEquals(0, Calculadora.multiplicar(0,5));
+    }
+    @Test
+    void multiplicar2() {
+        assertEquals(1000, Calculadora.multiplicar(1000,1));
+    }
+
+    @Test
+    void dividir(){
+        assertEquals(0, Calculadora.dividir(0,5));
+    }
+    @Test
+    void dividir2(){
+        assertEquals(0.1, Calculadora.dividir(1,10));
+    }
+
+
 }
