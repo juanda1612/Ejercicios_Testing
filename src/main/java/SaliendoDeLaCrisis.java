b48fd494d6b7de28d76a3397551b452e81742eab
juanda1612 <juan.perez77@educa.madrid.org>
2024-03-20 17:20:14 +0100

    asdasd

--- src/main/java/SaliendoDeLaCrisis.java
+++ src/main/java/SaliendoDeLaCrisis.java
@@ -4,12 +4,29 @@ public class SaliendoDeLaCrisis {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        int numeroLinea = teclado.nextInt();
+        int numeroLinea = teclado.nextInt(); teclado.nextLine();
+
         if (numeroLinea >= 3){
             while (numeroLinea != 0){
+               String[] numero = new String[numeroLinea];
+
                String cadena = teclado.nextLine();
-               String[] numeros = cadena.split(" ");
-               int[] numerosInt;
+               numero = cadena.split(" ");
+
+                int[] numerosInt = new int[numero.length];
+
+                for (int i = 0; i < numero.length; i++) {
+                    numerosInt [i] = Integer.parseInt(numero[i]);
+                }
+
+                for (int i = 0; i < numerosInt.length -1; i++) {
+                    if (numerosInt[i] < numerosInt[i+1]){
+                        System.out.println("NO");
+                    }else {
+                        System.out.println("SI");
+                    }
+                }
+                numeroLinea = teclado.nextInt();
             }
         }else {
             System.out.println("numero no valido");
