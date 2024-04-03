0430ebea3e8b10ffbc91af5f326220ab6ecf3303
juanda1612 <juan.perez77@educa.madrid.org>
2024-04-03 17:30:22 +0200

    assssssddsa

--- src/test/java/makigasJunit5/catalogo/PedidoTest.java
+++ src/test/java/makigasJunit5/catalogo/PedidoTest.java
@@ -1,9 +1,6 @@
 package makigasJunit5.catalogo;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,24 +19,41 @@ class PedidoTest {
     }
 
     @Test
-    void testCantidad() {
+    @DisplayName("Con 0 productos")
+    void testCantidad1() {
+        // para probar el metodo cantidad necesito un objeto de tipo Pedido con una serie de productos añadidos
+        Pedido p = new Pedido();
+        assertEquals(0, p.cantidad());
+
+    }
+
+    @Test
+    @DisplayName("Con 1 productos")
+    void testCantidad2() {
+        // para probar el metodo cantidad necesito un objeto de tipo Pedido con una serie de productos añadidos
+        Producto p1 = new Producto("Coco", 400);
+
+        Pedido p = new Pedido();
+
+        p.agregar(p1);
+        assertEquals(1, p.cantidad());
+    }
+
+    @Test
+    @DisplayName("Con 2 productos")
+    void testCantidad3() {
         // para probar el metodo cantidad necesito un objeto de tipo Pedido con una serie de productos añadidos
         Producto p1 = new Producto("Coco", 400);
         Producto p2 = new Producto("Canela", 200);
 
-        System.out.println("Prueba antes de meter productos");
+
         Pedido p = new Pedido();
-        assertEquals(0, p.cantidad());
 
-        System.out.println("Prueba despues de meter 1 productos");
         p.agregar(p1);
-        assertEquals(1, p.cantidad());
 
-        System.out.println("Prueba despues de meter 2 productos");
         p.agregar(p2);
         assertEquals(2, p.cantidad());
     }
-
     @Test
     void total() {
     }
