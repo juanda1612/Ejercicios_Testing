36d6ec0b47c0c96a0f96332385aca9360d9aa2d1
juanda1612 <juan.perez77@educa.madrid.org>
2024-04-22 16:27:53 +0200

    22-04-2024

---
+++ src/main/java/ejerciciosCajaNegra/ejercicio2/HarryPotter.java
@@ -0,0 +1,10 @@
+package ejerciciosCajaNegra.ejercicio2;
+
+public class HarryPotter {
+    public String nombre (String casa){
+        if (casa.equals("gryffindor")){
+            return "lealtad";
+        }
+        return null;
+    }
+}
